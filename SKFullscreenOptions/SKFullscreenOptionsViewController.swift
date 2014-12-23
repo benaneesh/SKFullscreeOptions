32d94c2d96f0e6284635b60b780811a81dfd4f7c
Ben Aneesh <benaneesh@gmail.com>
2014-12-23 16:19:17 -0500

    first commit

---
+++ SKFullscreenOptions/SKFullscreenOptionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SKFullscreenOptionsViewController.swift
+//  SKFullscreenOptions
+//
+//  Created by Ben Aneesh on 2014-12-23.
+//  Copyright (c) 2014 Ben Aneesh. All rights reserved.
+//
+
+import UIKit
+
+class SKFullscreenOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
